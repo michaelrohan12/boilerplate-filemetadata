30426e48b1248be33afb42dc1a13991da1f88426
michaelrscodes@gmail.com <michaelrscodes@gmail.com>
2023-12-11 18:27:14 +0530

    Updated Code with APIs

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer();
+require("dotenv").config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+//GET API to serve an html file
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// POST request to /api/fileanalyse to receive the file name, type, and size in bytes within the JSON response.
+app.route("/api/fileanalyse").post(upload.single("upfile"), (req, res) => {
+  try {
+    if (!req.file) {
+      throw new Error("No file uploaded");
+    }
 
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
-
+// App listening via port 3000
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, function (error) {
+  if (error) {
+    console.error("Error starting the server:", error);
+  } else {
+    console.log("Your app is listening on port " + port);
+  }
 });
